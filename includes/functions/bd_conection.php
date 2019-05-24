9c6e8af80233f15707fc82cd59d57986f272a55b
Federico <fserenelli@gmail.com>
2019-05-24 00:42:29 -0300

    correction

--- includes/functions/bd_conection.php
+++ includes/functions/bd_conection.php
@@ -1,5 +1,5 @@
 <?php  
-	$conn = new mysqli('localhost', 'root', 'root', 'gdlwebcamp');
+	$conn = new mysqli('localhost', 'root', 'root', 'gdlwebcamp2018');
 	if ($conn->connect_error) {
 		echo $error -> $conn->connect_error;
 	}
